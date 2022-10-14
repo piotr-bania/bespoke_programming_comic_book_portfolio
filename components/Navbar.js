02ee200a21f80bc918579ca4d246be4bb02eadc2
Piotr <piotr.bania@europe.com>
2022-10-14 16:40:36 +0100

    init

---
+++ components/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <section>
+        <header>
+            <Link href="/"><Image className='logo' src="/svg/logo.svg" alt="logo" width={200} height={81.365}/></Link>
+            {/* <nav>
+                <li>
+                    <Link href="/"><a>Home</a></Link>
+                    <Link href="/services"><a>Services</a></Link>
+                    <Link href="/process"><a>Process</a></Link>
+                    <Link href="/portfolio"><a>Portfolio</a></Link>
+                    <Link href="/blog"><a>Blog</a></Link>
+                    <Link href="/faq"><a>FAQ</a></Link>
+                </li>
+                <Link href="/quote"><button>Pricing</button></Link>
+            </nav> */}
+        </header>
+    </section>
+  )
+}
+
+export default Navbar
